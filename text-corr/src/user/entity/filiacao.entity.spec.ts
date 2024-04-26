bb4a8f1acffa88887d5f764791ae78fda2c020ec
ProfCarmino <73600424+ProfCarmino@users.noreply.github.com>
2024-04-26 08:22:03 -0300

    Delete text-corr directory

--- text-corr/src/user/entity/filiacao.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FiliacaoEntity } from './filiacao.entity';
-
-describe('FiliacaoEntity', () => {
-  it('should be defined', () => {
-    expect(new FiliacaoEntity()).toBeDefined();
-  });
-});
